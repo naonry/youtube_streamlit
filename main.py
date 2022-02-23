b6f9f024b2d1d3f80d7e6726c27cdc7135740d46
naonry <naonry88@gmail.com>
2022-02-23 17:56:03 +0900

    練習サイト更新

--- main.py
+++ main.py
@@ -1,10 +1,89 @@
 import streamlit as st
 import time
 
-st.title('Streamlit テストサイト')
+import pandas as pd
+import yfinance as yf
+import altair as alt
 
-st.write('プログレスバーの表示')
-'Start!!'
+st.title('可視化アプリ練習1')
+
+st.sidebar.write("""
+# GAFA株価
+こちらは株価可視化ツールです。以下のオプションから表示日数を指定してください""")
+
+st.sidebar.write("""
+## 表示日数選択
+# """)
+days = st.sidebar.slider('日数', 1, 50, 20)
+
+st.write(f"""
+### 過去　**{days}日間** のGAFA株価
+""")
+
+@st.cache
+def get_data(days, tickers):
+    df = pd.DataFrame()
+    for company in tickers.keys():
+        tkr = yf.Ticker(tickers[company])
+        hist = tkr.history(period=f'{days}d')
+        hist.index = hist.index.strftime('%d %B %Y')
+        hist = hist[['Close']]
+        hist.columns = [company]
+        hist = hist.T
+        hist.index.name = 'Name'
+        df = pd.concat([df, hist])
+    return df
+
+st.sidebar.write("""
+## 株価の範囲指定
+""")
+ymin, ymax = st.sidebar.slider(
+    '範囲を指定してください。',
+    0.0, 3500.0, (0.0, 3500.0)
+)
+
+tickers = {
+    'apple' : 'AAPL',
+    'facebook' : 'FB',
+    'google' : 'GOOGL',
+    'microsoft' : 'MSFT',
+    'netflix' : 'NFLX',
+    'amazon' : 'AMZN'
+}
+
+df = get_data(days, tickers)
+companies = st.multiselect(
+    '会社名を選択してください。',
+    list(df.index),
+    ['google','amazon','facebook','apple']
+)
+try:
+    if not companies:
+        st.error('少なくとも一社は選んでください。')
+    else:
+        data = df.loc[companies]
+        st.write("### 株価 (USD)", data.sort_index())
+        data = data.T.reset_index()
+        data = pd.melt(data, id_vars=['Date']).rename(
+            columns={'value': 'Stock Prices(USD)'}
+        )
+        chart = (
+            alt.Chart(data)
+            .mark_line(opacity=0.8, clip=True)
+            .encode(
+                x="Date:T",
+                y=alt.Y("Stock Prices(USD):Q", stack=None, scale=alt.Scale(domain=[ymin, ymax])),
+                color='Name:N'
+            )
+        )
+        st.altair_chart(chart, use_container_width=True)
+except:
+    st.error(
+        "おっと！なにかエラーが起きているようです。"
+    )
+  
+
+
 
 latest_iteration = st.empty()
 bar = st.progress(0)
@@ -16,14 +95,6 @@ for i in range(100):
 
 'Done!!!!!'
 
-
-
-# st.write(
-#     pd.DataFrame({
-#         '1st column': [1,2,3,4],
-#         '2st column': [10,20,30,40]
-#     })
-# )
 
 """
 # レベル1 My 1st App
@@ -44,17 +115,6 @@ library('brms')
 ```
 """
 
-
-
-# df = pd.DataFrame(
-#     np.random.rand(100, 2)/[50, 50]+[35.69, 139.70],
-#     columns = [ 'lat', 'lon']
-# )
-
-# st.map(df)
-
-# st.write('Display Image')
-
 left_column, right_column = st.columns(2)
 button = left_column.button('右カラムに文字を表示')
 if button:
@@ -63,21 +123,3 @@ if button:
 expander = st.expander('問い合わせ')
 expander.write('問い合わせ内容を書く')
 
-
-# option = st.selectbox(
-#     'あなたが好きな数字を教えてください',
-#     list(range(1,11))
-# )
-# 'あなたの好きな数字は、', option , 'です。'
-
-# if st.checkbox('Show Image'):
-#     img = Image.open('IMG_2198.JPG')
-#     st.image(img, caption='こまち', use_column_width = True)
-
-# st.write('Interactive Widgets')
-
-# text = st.text_input('あなたの趣味を教えてください')
-# st.write('あなたの趣味:', text)
-
-# condition = st.slider('あなたの今の調子は', 0, 100, 50)
-# st.write('あなたの調子は:', condition)
